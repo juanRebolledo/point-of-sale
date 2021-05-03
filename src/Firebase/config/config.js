db9911f6c714c5e4b65d005628dc8f4030226785
Jose Juan Rebolledo Kamey <jrebolledo4@ucol.mx>
2021-05-02 20:14:43 -0500

    env

--- src/Firebase/config/config.js
+++ src/Firebase/config/config.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyA65WcggW9lBuKuiOBkoDon8PL2XZyoUcY',
+  apiKey: process.env.APIKEY,
   authDomain: 'point-of-sale-daa6e.firebaseapp.com',
   databaseURL: 'https://point-of-sale-daa6e-default-rtdb.firebaseio.com/',
   projectId: 'point-of-sale-daa6e',
--- src/views/public/Login/Login.jsx
+++ src/views/public/Login/Login.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import cashRegisterImage from 'assets/img/retailer.png'
 import LoginForm from './components/LoginForm/LoginForm'
 
 const LoginView = () => {
   return (
     <div className="h-100 w-100 d-flex justify-content-center align-items-center">
+      <img alt="punto de venta" src={cashRegisterImage} />
       <LoginForm />
     </div>
   )
