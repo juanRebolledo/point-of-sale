58905929b3565625d6f21789bb7567e5573353a0
Jose Juan Rebolledo Kamey <jrebolledo4@ucol.mx>
2021-05-09 18:07:26 -0500

    login view

--- src/views/public/Login/Login.jsx
+++ src/views/public/Login/Login.jsx
@@ -5,7 +5,6 @@ import LoginForm from './components/LoginForm/LoginForm'
 const LoginView = () => {
   return (
     <div className="h-100 w-100 d-flex justify-content-center align-items-center">
-      <img alt="punto de venta" src={cashRegisterImage} />
       <LoginForm />
     </div>
   )
