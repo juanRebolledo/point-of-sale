0aa624a65caf0ead99741d54986893ec01d9d3b9
Jose Juan Rebolledo Kamey <jrebolledo4@ucol.mx>
2021-05-09 17:47:45 -0500

    routes fixed

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -14,17 +14,20 @@ import PublicRoute from './public/PublicRoute'
 import PrivateRoute from './private/PrivateRoute'
 
 function AppRouter() {
-  const { auth } = useContext(UserContext)
+  const { auth, defaultStore } = useContext(UserContext)
+
+  const isAuthToApp = auth && defaultStore.length > 0
+
   return (
     <Switch>
       <PublicRoute path="/" component={LoginView} auth={auth} exact />
       <PrivateRoute path="/admin/inicio" component={HomeView} auth={auth} exact />
-      <PrivateRoute path="/admin/:id/ganancias" component={ProfitsView} auth={auth} exact />
-      <PrivateRoute path="/admin/:id/inventario/:id" component={ProductsView} auth={auth} exact />
-      <PrivateRoute path="/admin/:id/inventario" component={InventoryView} auth={auth} exact />
-      <PrivateRoute path="/admin/:id/proveedores" component={ProvidersView} auth={auth} exact />
-      <PrivateRoute path="/admin/:id/usuarios" component={UsersView} auth={auth} exact />
-      <PrivateRoute path="/admin/:id/vender" component={MarketView} auth={auth} exact />
+      <PrivateRoute path="/admin/:id/ganancias" component={ProfitsView} auth={isAuthToApp} exact />
+      <PrivateRoute path="/admin/:id/inventario/:id" component={ProductsView} auth={isAuthToApp} exact />
+      <PrivateRoute path="/admin/:id/inventario" component={InventoryView} auth={isAuthToApp} exact />
+      <PrivateRoute path="/admin/:id/proveedores" component={ProvidersView} auth={isAuthToApp} exact />
+      <PrivateRoute path="/admin/:id/usuarios" component={UsersView} auth={isAuthToApp} exact />
+      <PrivateRoute path="/admin/:id/vender" component={MarketView} auth={isAuthToApp} exact />
       <Route path="*" component={NotFound} />
     </Switch>
   )
